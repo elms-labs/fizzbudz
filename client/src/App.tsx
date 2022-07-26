79cdd182839aa1544bd8cbb419405b50b90e276b
Sattwik <hellosattwik@gmail.com>
2022-07-27 00:58:47 +0530

    setup tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
+      <div className='bg-slate-400 flex items-center justify-center'>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
