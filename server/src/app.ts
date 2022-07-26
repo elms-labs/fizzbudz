54dee2a7ff59b093a9dea77fdef5e3e609d01009
Sattwik <hellosattwik@gmail.com>
2022-07-27 00:47:38 +0530

    setup client and server

---
+++ server/src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, NextFunction, RequestHandler } from 'express';
+// import { playerController } from './playerController';
+
+const app = express();
+
+app.use(express.json());
+
+// app.get('/api', playerController.getScores, (req, res, next) => res.json(res.locals.scores));
+// app.post('/api', playerController.updateScores, (req, res, next) => res.json(res.locals.scores));
+
+type ServerError = {
+    log: string,
+    status: number,
+    message: {err: string}
+}
+
+app.use('/', (err: ServerError, req: Request, res: Response, next: NextFunction) => {
+  const defaultErr: ServerError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+})
+
+app.listen(3000, () => console.log('server is listening on port 3000'));
