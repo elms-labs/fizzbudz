174f97523aebaf7035a71aa5ef267b7f7c7c5e34
Miranda Jaramillo Morales <mirandajaramillo@MacBook-Pro-de-Miranda-2.local>
2022-07-27 16:12:34 -0500

    Created skills route and component, with skillset from an engineer

---
+++ client/src/components/Skills.tsx
@@ -0,0 +1,140 @@
+import fizz from '../assets/fizz.png';
+
+function Skills(){
+    return( 
+    <div>
+
+<div className="p-8 flex align-center justify-center ">
+  <div className="container-fluid p-8 border border-l-8 border-coding-red shadow-xl rounded-lg bg-gray-100 mt-80">
+  
+  <h3 className= "text-3xl font-sbold ">Show us your skillset!</h3>
+    
+    <div className="h-auto w-auto p-6 mt-6 rounded-lg bg-slate-300 border border-slate-300 grid grid-cols-4 gap-4">
+
+
+{/* Recursion */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+        Recursion
+      </label>
+    </div>
+  </div>
+</div>
+
+{/* Closure */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+        Closure
+      </label>
+    </div>
+  </div>
+</div>
+
+
+{/* React */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+        React
+      </label>
+    </div>
+  </div>
+</div>
+
+{/* Node.js */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+        Node.js
+      </label>
+    </div>
+  </div>
+</div>
+
+
+{/* OAuth */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+        OAuth
+      </label>
+    </div>
+  </div>
+</div>
+
+
+{/* Big O Notation */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+        Big O Notation
+      </label>
+    </div>
+  </div>
+</div>
+
+
+{/* Data Structures */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+        Data Structures 
+      </label>
+    </div>
+  </div>
+</div>
+
+{/* Being cool */}
+<div className="flex">
+  <div>
+    <div className="form-check">
+      <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-coding-red checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault"></input>
+      <label className="form-check-label inline-block text-gray-800" >
+       Being cool 😎 
+      </label>
+    </div>
+  </div>
+</div>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+
+
+    </div>
+   )
+}
+
+
+export default Skills;
